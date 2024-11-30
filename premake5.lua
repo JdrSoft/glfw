6cb20d10721140a18eca9c4e3eb15e62fd49dc72
Jordy <jordyderon@gmail.com>
2024-11-30 18:51:44 +0100

    update premake5 file

--- premake5.lua
+++ premake5.lua
@@ -1,62 +1,55 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-    staticruntime "off"
-    warnings "off"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
+	files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
+        "src/internal.h",
+        "src/platform.h",
+        "src/mappings.h",
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/platform.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
-
-    filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
+    
+	filter "system:windows"
         systemversion "latest"
         staticruntime "On"
-
+        
         files {
             "src/win32_init.c",
+            "src/win32_module.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_time.h",
             "src/win32_time.c",
+            "src/win32_thread.h",
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c"
         }
-
-        defines {
+		defines { 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-        }
-    
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "On"
+		}
 
-    filter { "system:windows", "configurations:Debug-AS" }	
-		runtime "Debug"
-		symbols "on"
-		sanitize { "Address" }
-		flags { "NoRuntimeChecks", "NoIncrementalLink" }
-
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "Speed"
-        
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "Speed"
-        symbols "Off"
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
